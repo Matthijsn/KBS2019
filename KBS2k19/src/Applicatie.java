0f5a9d1b423836d5038960d60a6ab2025b301b33
Matthijsn <Matthijsn@users.noreply.github.com>
2019-05-09 09:42:59 +0200

    ..

--- KBS2k19/src/Applicatie.java
+++ KBS2k19/src/Applicatie.java
@@ -1,10 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.util.ArrayList;
 
-public class Applicatie extends JFrame implements ActionListener{
+public class Applicatie extends JFrame implements ActionListener, MouseListener {
     private JButton jbbestand;
     private JLabel jlkosten;
     private JLabel jlbeschikbaarheid;
@@ -14,10 +13,16 @@ public class Applicatie extends JFrame implements ActionListener{
     private ArrayList<Component> Componenten;
     private ArrayList<DBServer> DBServers;
     private ArrayList<Webserver> Webservers;
-//    private JButton
+    private String[] types = {"DBServer", "Webserver", "Loadbalancer    ", "Firewall"};
+    private JList<String> cb = new JList<>(types);
+    private String[] DBS = {"HAL9001DB", "HAL9002DB", "HAL9003DB", " "};
+    JList<String> DB = new JList<>(DBS);
+    private String Input;
+    //    private JButton
     Panel Panel = new Panel();
+    JFrame frame = new JFrame();
 
-    public Applicatie(){
+    public Applicatie() {
         Componenten = new ArrayList<>();
         DBServers = new ArrayList<>();
         Webservers = new ArrayList<>();
@@ -25,7 +30,7 @@ public class Applicatie extends JFrame implements ActionListener{
         DBServers.add(HAL9001DB);
         DBServer HAL9002DB = new DBServer("HAL9002DB", 95, 7700);
         DBServers.add(HAL9002DB);
-        DBServer HAL9003DB = new DBServer("HAL9003DB", 98, 122000)
+        DBServer HAL9003DB = new DBServer("HAL9003DB", 98, 122000);
         DBServers.add(HAL9003DB);
         Webservers.add(new Webserver("HAL9001W", 80, 2200));
         Webservers.add(new Webserver("HAL9002W", 90, 3200));
@@ -35,42 +40,57 @@ public class Applicatie extends JFrame implements ActionListener{
         Componenten.add(new Firewall("pfSense", 99.999, 2000));
         Componenten.add(new Loadbalancer("DBloadbalancer", 99.999, 2000));
 
-        setTitle("Infrastructuur Applicatie");
-        setSize(800,400);
-        setLayout(new FlowLayout());
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Infrastructuur Applicatie");
+        frame.setSize(800, 400);
+        frame.setLayout(new FlowLayout());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //bestand button
         jbbestand = new JButton("Bestand");
-        add(jbbestand);
+        frame.add(jbbestand);
         //jbbestand.addActionListener(this);
-        jbbestand.setPreferredSize(new Dimension(150,50));
+        jbbestand.setPreferredSize(new Dimension(150, 50));
         jbbestand.setAlignmentX(LEFT_ALIGNMENT);
 
         jlkosten = new JLabel("Kosten: ");
-        add(jlkosten);
-        jlkosten.setPreferredSize(new Dimension(200,50));
+        frame.add(jlkosten);
+        jlkosten.setPreferredSize(new Dimension(200, 50));
 
         jlbeschikbaarheid = new JLabel("Beschikbaarheid: ");
-        add(jlbeschikbaarheid);
-        jlbeschikbaarheid.setPreferredSize(new Dimension(200,50));
+        frame.add(jlbeschikbaarheid);
+        jlbeschikbaarheid.setPreferredSize(new Dimension(200, 50));
 
         jbberekenKosten = new JButton("Bereken Kosten");
-        add(jbberekenKosten);
-        String[] types = { "DBServer","Webserver", "Loadbalancer    ","Firewall"};
+        jbberekenKosten.setPreferredSize(new Dimension(200, 50));
+        frame.add(jbberekenKosten);
+        //        String[] DBS = {" "};
 
-        final JList<String> cb = new JList<>(types);
-        final JList<DBServer> DBS = new JList<>(DBServers);
-        add(cb);
-//        cb.ad
-        jbberekenKosten.setPreferredSize(new Dimension(200,50));
-
-        add(Panel);
-
-
-        setVisible(true);
+        String[] WS = {"HAL9001W", "HAL9002W", "HAL9003W", " "};
+        frame.add(cb);
+        if(Input == "DBServer"){
+            frame.add(DB);
+        }
+        MouseListener mouseListener = new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                    System.out.println(cb.getSelectedValue());
+            }
+        };
+        System.out.println("hoii");
+        cb.addMouseListener(mouseListener);
+        frame.add(Panel);
+        frame.setVisible(true);
     }
-    public void actionPerformed(ActionEvent e){
 
+    public void actionPerformed(ActionEvent e){
+    }
+    public void mousePressed(MouseEvent e){
+    }
+    public void mouseClicked(MouseEvent e){
+    }
+    public void mouseReleased(MouseEvent e){
+    }
+    public void mouseEntered(MouseEvent e){
+    }
+    public void mouseExited(MouseEvent e){
     }
 }
